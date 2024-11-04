ddca5d2533bec0cca4a7137cd3787269f775d04d
Alexis-Valles <L24550808@chihuahua2.tecnm.mx>
2024-11-04 16:52:03 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author alexi
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+        //INVOCAR EL METODO; LLAMA A METODO --> USARLO
+        imprimirMensaje("Hola mundo!!");
+    }
+    // declaracion del metodo
+    //Modificadores
+    //Valor de retorno
+    //Nombre
+    //Parametros (opcionales)
+    public static void imprimirMensaje(String mensaje){
+        //Cuerpo del metodo
+        System.out.println("Tu mensaje: " + mensaje);
+    }
+       
+}
